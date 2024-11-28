3ca3a5bfe95cde384498656c63af551ec7df5db9
mfalkner <mofalknerhakimst.rocks>
2024-11-28 14:31:35 +0100

    Nun wurde die Methode ausgearbeitet und es wurde in der Main auch schon die Verbindung zur Datenbank hergestellt!

--- src/main/java/at/hakimst/Main.java
+++ src/main/java/at/hakimst/Main.java
@@ -1,7 +1,25 @@
 package at.hakimst;
 
+import dataaccess.MysqlDatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST");
+        try
+        {
+            Connection myConnection =
+                    MysqlDatabaseConnection.getConnection("jdbc:mysql:// localhost:3306/kurssystem","root","");
+            System.out.println("Verbindung aufgebaut!");
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
