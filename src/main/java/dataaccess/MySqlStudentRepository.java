081b41e5a038bb9f0ee057e973ab83dcc7f69ad2
mfalkner <mofalknerhakimst.rocks>
2024-12-12 10:38:36 +0100

    Es wurde in der Klasse MySqlStudentRepository ein Fehler ausgebessert!

--- src/main/java/dataaccess/MySqlStudentRepository.java
+++ src/main/java/dataaccess/MySqlStudentRepository.java
@@ -93,41 +93,7 @@ public class MySqlStudentRepository implements MyStudentRepository {
 
     @Override
     public Optional<Student> getById(Long id) {
-        Assert.notNull(id);
-        if (countStudentsInDbWithId(id) == 0) {
-            return Optional.empty();
-        } else {
-            try {
-                String sql = "SELECT * FROM `students` WHERE `id` = ?";
-                PreparedStatement preparedStatement = con.prepareStatement(sql);
-                preparedStatement.setLong(1, id);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                resultSet.next();
-                Student student = new Student(
-                        resultSet.getLong("id"),
-                        resultSet.getString("vn"),
-                        resultSet.getDate("geburtstag"),
-                        resultSet.getString("nn")
-                );
-                return Optional.of(student);
-            } catch (SQLException sqlException) {
-                throw new DatabaseException(sqlException.getMessage());
-            }
-        }
-    }
-
-    private int countStudentsInDbWithId(Long id) {
-        try {
-            String countSql = "SELECT COUNT(*) FROM `students` WHERE `id`=?";
-            PreparedStatement preparedStatementCount = con.prepareStatement(countSql);
-            preparedStatementCount.setLong(1, id);
-            ResultSet resultSetCount = preparedStatementCount.executeQuery();
-            resultSetCount.next();
-            int courseCount = resultSetCount.getInt(1);
-            return courseCount;
-        } catch (SQLException sqlException) {
-            throw new DatabaseException(sqlException.getMessage());
-        }
+        return Optional.empty();
     }
 
     @Override
